831c5e060dc49b6dec6fa3ba9da320fe4d37950f
R Megha <Megha.R2@cerner.com>
2020-09-26 17:11:46 +0530

    Duplicate.swift

---
+++ Duplicate.swift
@@ -0,0 +1,19 @@
+// cerner_2^5_2020
+import Foundation
+extension Array where Element: Hashable
+ {
+    var uniques: Array
+    {
+        var buffer = Array()
+        var added = Set<Element>()
+        for elem in self 
+        {
+            if !added.contains(elem) 
+            {
+                buffer.append(elem)
+                added.insert(elem)
+            }
+        }
+        return buffer
+    }
+}
